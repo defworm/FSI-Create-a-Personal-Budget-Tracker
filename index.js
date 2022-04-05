198b1e2ad6bcf631837251ed90c42fbafe395ed2
Crystal Korth <crystalkorth@gmail.com>
2022-04-05 13:39:47 -0500

    questions added, works in web

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+//let question = Number(window.prompt 
+  //  (weeklyExpenseQuestions[0]))
+//console.log(question)
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++) {
+    let question = Number(window.prompt 
+        (weeklyExpenseQuestions[i]))
+        weeklyExpenses = weeklyExpenses + question
+}
+for(let i = 0; i < monthlyExpenseQuestions.length; i++) {
+    let question = Number(window.prompt 
+        (monthlyExpenseQuestions[i]))
+        monthlyExpenses = monthlyExpenses + question
+}
+for(let i = 0; i < annualExpenseQuestions.length; i++) {
+    let question = Number(window.prompt 
+        (annualExpenseQuestions[i]))
+        annualExpenses = annualExpenses + question
+}
